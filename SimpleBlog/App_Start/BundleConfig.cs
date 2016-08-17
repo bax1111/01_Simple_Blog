5b12b7bcb6c904c81bbb7d9e2db11b6bbaa62b11
Willy Anicette <bax1111@gmail.com>
2016-08-17 00:20:00 -0400

    Added transaction filter Added basic admin

--- SimpleBlog/App_Start/BundleConfig.cs
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace SimpleBlog
                 .Include("~/scripts/jquery-3.1.0.js")
                 .Include("~/scripts/jquery.validate.js")
                 .Include("~/scripts/jquery.validate.unobtrusive.js")
-                .Include("~/scripts/bootstrap.js"));
+                .Include("~/scripts/bootstrap.js")
+                .Include("~/areas/admin/scripts/forms.js"));
 
             bundles.Add(new ScriptBundle("~/scripts")
                 .Include("~/scripts/jquery-3.1.0.js")
@@ -28,6 +29,6 @@ namespace SimpleBlog
                 .Include("~/scripts/jquery.validate.unobtrusive.js")
                 .Include("~/scripts/bootstrap.js"));
         }
-         
+
     }
 }
--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -14,7 +14,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
     [SelectedTab("users")]
     public class UsersController : Controller
     {
-        
+
         public ActionResult Index()
         {
             return View(new UsersIndex
@@ -23,8 +23,123 @@ namespace SimpleBlog.Areas.Admin.Controllers
             });
         }
 
-        
+        public ActionResult New()
+        {
+            return View(new UsersNew
+            {
+
+            });
+
+        }
+
+        [HttpPost]
+        public ActionResult New(UsersNew form)
+        {
+            if (Database.Session.Query<User>().Any(u => u.Username == form.Username))
+                ModelState.AddModelError("Username", "Username must be unique");
+
+            if (!ModelState.IsValid)
+                return View(form);
+            var user = new User
+            {
+                Email = form.Email,
+                Username = form.Username,
+
+            };
+
+            user.SetPassword(form.Password);
+
+            Database.Session.Save(user);
+
+
+            return RedirectToAction("index");
+
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var user = Database.Session.Load<User>(id);
+            if (user == null)
+                return HttpNotFound();
+
+            return View(new UsersEdit
+            {
+                Username = user.Username,
+                Email = user.Email
+            });
+
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(int id, UsersEdit form)
+        {
+            var user = Database.Session.Load<User>(id);
+            if (user == null)
+                return HttpNotFound();
+
+            if (Database.Session.Query<User>().Any(u => u.Username == form.Username && u.Id != id))
+                ModelState.AddModelError("Username", "Username must be unique");
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            user.Username = form.Username;
+            user.Email = form.Email;
+
+            Database.Session.Update(user);
+
+            return RedirectToAction("index");
+        }
+
+
+        public ActionResult ResetPassword(int id)
+        {
+            var user = Database.Session.Load<User>(id);
+            if (user == null)
+                return HttpNotFound();
+
+            return View(new UsersResetPassword
+            {
+                Username = user.Username
+
+            });
+        }
+
+        [HttpPost]
+        public ActionResult ResetPassword(int id, UsersResetPassword form)
+        {
+            var user = Database.Session.Load<User>(id);
+            if (user == null)
+                return HttpNotFound();
+
+            form.Username = user.Username;
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            user.SetPassword(form.Username);
+
+            Database.Session.Update(user);
+
+            return RedirectToAction("index");
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var user = Database.Session.Load<User>(id);
+            if (user == null)
+                return HttpNotFound();
+
+            Database.Session.Delete(user);
+            return RedirectToAction("Index");
+        }
+
     }
 
-    
+
 }
